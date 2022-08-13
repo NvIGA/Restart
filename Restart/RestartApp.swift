2d03f9c0f7cc0fea2d812957b09a8b530462f073
NvIGA <devprogram.w@gmail.com>
2022-08-13 19:32:26 +0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Volodymyr Taratunskyi on 13.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
